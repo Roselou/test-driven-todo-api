460ae7d14b5cc93c6bfa1bc9114057c79c69c13a
Roselou <elizabethroselou@gmail.com>
2018-03-27 12:18:53 -0700

    in-class api work

--- server.js
+++ server.js
@@ -9,110 +9,118 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // serve static files from public folder
 app.use(express.static(__dirname + '/public'));
 
-/************
- * DATABASE *
- ************/
+// /************
+//  * DATABASE *
+//  ************/
 
-// our database is an array for now with some hardcoded values
+// // our database is an array for now with some hardcoded values
 var todos = [
   { _id: 7, task: 'Laundry', description: 'Wash clothes' },
   { _id: 27, task: 'Grocery Shopping', description: 'Buy dinner for this week' },
   { _id: 44, task: 'Homework', description: 'Make this app super awesome!' }
 ];
 
-/**********
- * ROUTES *
- **********/
+// /**********
+//  * ROUTES *
+//  **********/
 
-/*
- * HTML Endpoints
- */
+// /*
+//  * HTML Endpoints
+//  */
 
 app.get('/', function homepage(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
-/*
- * JSON API Endpoints
- *
- * The comments below give you an idea of the expected functionality
- * that you need to build. These are basic descriptions, for more
- * specifications, see the todosTest.js file and the outputs of running
- * the tests to see the exact details. BUILD THE FUNCTIONALITY IN THE
- * ORDER THAT THE TESTS DICTATE.
- */
+// /*
+//  * JSON API Endpoints
+//  *
+//  * The comments below give you an idea of the expected functionality
+//  * that you need to build. These are basic descriptions, for more
+//  * specifications, see the todosTest.js file and the outputs of running
+//  * the tests to see the exact details. BUILD THE FUNCTIONALITY IN THE
+//  * ORDER THAT THE TESTS DICTATE.
+//  */
 
 // app.get('/api/todos/search', function search(req, res) {
-//    This endpoint responds with the search results from the
-//    * query in the request. COMPLETE THIS ENDPOINT LAST.
+//    // This endpoint responds with the search results from the
+//    // * query in the request. COMPLETE THIS ENDPOINT LAST.
    
 // });
 
 app.get('/api/todos', function index(req, res) {
    // This endpoint responds with all of the todos
-   res.json(todos)
+   res.json({data: todos})
 });
 
 app.post('/api/todos', function create(req, res) {
-  /* This endpoint will add a todo to our "database"
-   * and respond with the newly created todo.
-   */
-    // var newToDo =
-    //   { _id: 88, task: 'Pay bills', description: 'Rent, internet and PG&E for upcoming month' };
-    var number = req.body._id;
-    var duty = req.body.task;
-    var info = req.body.description;
-    var newToDo = {_id: number, task: duty, description: info};
-    todos.push(newToDo);
-    res.jsonp(newToDo);
-});
+   // This endpoint will add a todo to our "database"
+   // * and respond with the newly created todo.
+  
+   
+    var lastToDo = todos[todos.length-1];
+    var nextId = lastToDo._id+1;
+   
+   var newToDo = {
+    _id: nextId,
+    task: req.body.task,
+    description: req.body.description
+   }
+   todos.push(newToDo)
 
+   res.json(newToDo)
+});
 
 app.get('/api/todos/:id', function show(req, res) {
   /* This endpoint will return a single todo with the
    * id specified in the route parameter (:id)
    */
-   var todosId =.params._id;
-   var appropriateId = todos.filter(function(todosObj){
-      return todosObj.id == todosId;
+   
+   var todosToFind = req.params.id;
+   var appropriateId = todos.find(function(todosObj){
+      return todosObj._id == todosToFind
    });
-   response.send(appropriateId);
+   res.send(appropriateId);
 });
 
 app.put('/api/todos/:id', function update(req, res) {
   /* This endpoint will update a single todo with the
    * id specified in the route parameter (:id) and respond
    * with the newly updated todo.
-   */
-     var id = req._id;
-
-     var id = _.extend(id, req.body._id);
-
-     id.save(function(err){
-     if (err){
-        return res.send('/id',{
-        errors: err.errors,
-        id: id
+//    */
+     var idToFind = req.params.id;
+     todos.forEach(function(foundToDo){
+      if (eachTodo._id === todoIdToUpdate){
+       foundToDo.task = req.body.task
+       foundToDo.description = req.body.description
+      } 
+      return foundToDo
      });
-   } else {
-    res.jsonp(id);
-   }
+     res.json(foundToDo)
+ });
 
-});
+// //     
 
 app.delete('/api/todos/:id', function destroy(req, res) {
   /* This endpoint will delete a single todo with the
    * id specified in the route parameter (:id) and respond
    * with success.
-   */
-    var todosId = params._id;
-    res.json(msg: "id deleted")
+//    */
+  var idInRoute = req.params.id; 
+  todos = todos.filter(function(deletedTodo){
+    if (idInRoute != deletedTodo._id){
+      return todos 
+    }
+  });
+  res.send("success");
+// //     var todosId = params._id;
+// //     res.json(msg: "id deleted")
 });
 
-/**********
- * SERVER *
- **********/
+// /**********
+//  * SERVER *
+//  **********/
 
 // listen on port 3000
 app.listen(3000, function() {
